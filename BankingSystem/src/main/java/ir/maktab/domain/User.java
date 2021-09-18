15b997c279129087358b67eb640d3dbdcfca9ebc
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-19 02:33:28 +0430

    Add User

---
+++ BankingSystem/src/main/java/ir/maktab/domain/User.java
@@ -0,0 +1,69 @@
+package ir.maktab.domain;
+
+import ir.maktab.base.domain.BaseEntity;
+import ir.maktab.domain.enumeration.UserType;
+import lombok.*;
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseEntity<Long> {
+
+    public static final String TABLE_NAME = "user_table";
+    public static final String FIRST_NAME = "first_name";
+    public static final String LAST_NAME = "last_name";
+    public static final String USER_NAME = "user_name";
+    public static final String PASSWORD = "password";
+    public static final String USER_TYPE = "user_type";
+    public static final String MOBILE_NUMBER = "mobile_number";
+    public static final String EMAIL = "email";
+    public static final String BIRTH_DATE = "birth_date";
+    public static final String USER_ID = "user_id";
+
+    @Column(name = FIRST_NAME)
+    private String firstName;
+
+    @Column(name = LAST_NAME)
+    private String lastName;
+
+    @Column(name = USER_NAME, unique = true)
+    private String username;
+
+    @Column(name = PASSWORD)
+    private String password;
+
+    @Column(name = USER_TYPE)
+    @Enumerated(value = EnumType.STRING)
+    private UserType userType;
+
+    @ElementCollection
+    @CollectionTable(name = MOBILE_NUMBER, joinColumns = @JoinColumn(name = USER_ID))
+    private Set<String> mobileNumber = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = EMAIL, joinColumns = @JoinColumn(name = USER_ID))
+    private Set<String> email = new HashSet<>();
+
+    @Column(name = BIRTH_DATE)
+    private ZonedDateTime birthDate;
+
+    private Boolean isActive;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", userType='" + userType + '\'' +
+                '}';
+    }
+}
