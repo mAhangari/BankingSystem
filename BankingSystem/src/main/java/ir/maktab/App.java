b5fdae0da0bf60ea720381f87a92e0f636edb54c
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-19 22:53:15 +0430

    Modify App

---
+++ BankingSystem/src/main/java/ir/maktab/App.java
@@ -0,0 +1,9 @@
+package ir.maktab;
+
+import ir.maktab.util.ApplicationContext;
+
+public class App {
+    public static void main( String[] args ) {
+        ApplicationContext.menu.showMenu();
+    }
+}
