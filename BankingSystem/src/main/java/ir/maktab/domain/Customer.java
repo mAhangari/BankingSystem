8b64bbe4be20c80960fc97e358508114d406d1be
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-19 21:54:49 +0430

    Add Customer

---
+++ BankingSystem/src/main/java/ir/maktab/domain/Customer.java
@@ -0,0 +1,69 @@
+package ir.maktab.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+
+//@NamedEntityGraph(name = Customer.FETCH_TWEET_AND_COMMENT,
+//			attributeNodes = {
+//					@NamedAttributeNode(value = "tweets", subgraph = "commentOfTweet"),
+//			},
+//			subgraphs = {
+//					@NamedSubgraph(name = "commentOfTweet",
+//							attributeNodes = {
+//									@NamedAttributeNode(value = "comments")
+//							}
+//						)
+//			}
+//
+//		)
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Customer extends User {
+	
+	public static final String FETCH_TWEET_AND_COMMENT = "FETCH_TWEET_AND_COMMENT";
+	public static final String BIRTHDAY = "birthday";
+
+	@Column(name = BIRTHDAY)
+	private Date birthday;
+	
+	@OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Set<Account> accounts = new HashSet<>();
+
+	public Customer(String username, String password, String firstName,
+			String lastName) {
+		super.setUsername(username);
+		super.setPassword(password);
+		super.setFirstName(firstName);
+		super.setLastName(lastName);
+		//super.setIsActive(isActive);
+		//this.setBirthday(birthday);
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	
+	@Override
+	public String toString() {
+		return  "id='" + getId() + "' " +
+                "firstName= '" + getFirstName() + "' " +
+                "lastName= '" + getLastName() + "' " +
+                "username= '" + getUsername() + "' " +
+                "password= '" + getPassword() + "' " +
+                "birthday= '" + birthday + "' ";
+	}
+}
