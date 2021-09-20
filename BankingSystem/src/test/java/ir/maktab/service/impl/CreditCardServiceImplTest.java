54e102d53628147129830b35a47d682cba9f8064
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-21 01:43:03 +0430

    Add CreditCardServiceImplTest

---
+++ BankingSystem/src/test/java/ir/maktab/service/impl/CreditCardServiceImplTest.java
@@ -0,0 +1,16 @@
+package ir.maktab.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreditCardServiceImplTest {
+
+    @Test
+    void cardToCartTransfer() {
+    }
+
+    @Test
+    void assignOrChangePassword() {
+    }
+}
