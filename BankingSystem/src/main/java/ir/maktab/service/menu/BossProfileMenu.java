60d537ded4cbfb7480ba56ebc18242d6f35027f6
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-20 21:57:36 +0430

    Modify BossProfileMenu

--- BankingSystem/src/main/java/ir/maktab/service/menu/BossProfileMenu.java
+++ BankingSystem/src/main/java/ir/maktab/service/menu/BossProfileMenu.java
@@ -1,13 +1,16 @@
 package ir.maktab.service.menu;
 
+import ir.maktab.domain.Boss;
+import ir.maktab.domain.User;
+
 import java.util.*;
 
-public class BossProfileMenu implements ProfileMenu {
+public class BossProfileMenu implements ProfileMenu<Boss> {
 
     Scanner input = new Scanner(System.in);
 
     @Override
-    public void dashboard() {
+    public void dashboard(User boss) {
         try {
             List<String> list = new ArrayList<>(
                     Arrays.asList("", "Log Out"));
@@ -18,13 +21,13 @@ public class BossProfileMenu implements ProfileMenu {
                 }
                 default -> {
                     BaseMenu.singlePrintMessage(BaseMenu.WRONG_NUMBER);
-                    dashboard();
+                    dashboard(boss);
                 }
             }
         } catch (InputMismatchException e) {
             input.nextLine();
             BaseMenu.singlePrintMessage(BaseMenu.WRONG_NUMBER);
-            dashboard();
+            dashboard(boss);
         }
     }
 }
