5ddb1923585c26d2cb194ee2ea7cca464cd58c57
MORTEZA <morteza_ahangari_1994@yahoo.com>
2021-09-21 15:37:36 +0430

    Modify ProfileMenu and ProfileMenuFactory change parameter type

--- BankingSystem/src/main/java/ir/maktab/service/menu/ProfileMenuFactory.java
+++ BankingSystem/src/main/java/ir/maktab/service/menu/ProfileMenuFactory.java
@@ -1,13 +1,13 @@
 package ir.maktab.service.menu;
 
-import ir.maktab.domain.User;
+import ir.maktab.domain.IUser;
 import ir.maktab.domain.enumeration.UserType;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ProfileMenuFactory {
 
-    private static final Map<UserType, ProfileMenu<? extends User>> profileMenus = new HashMap<>() {
+    private static final Map<UserType, ProfileMenu<? extends IUser>> profileMenus = new HashMap<>() {
         {
             put(UserType.Customer, new CustomerProfileMenu());
             put(UserType.Employee, new EmployeeProfileMenu());
@@ -15,7 +15,7 @@ public class ProfileMenuFactory {
         }
     };
 
-    public static ProfileMenu<? extends User> getProfileMenu(User user) {
+    public static ProfileMenu<? extends IUser> getProfileMenu(IUser user) {
 
         return profileMenus.get(user.getUserType());
     }
